a6dc535f925c0a4d80ef47f264c66a29d848b193
Edward Fouad <edward.fouad@gmail.com>
2022-05-07 20:28:28 -0400

    Added rendering to discrete 2 dof arm

--- gym-simulations/gym_simulations/envs/discrete_2dof.py
+++ gym-simulations/gym_simulations/envs/discrete_2dof.py
@@ -11,7 +11,7 @@ from gym import spaces
 import numpy as np
 
 class Discrete2DoF(gym.Env):
-    metadata = {"render_modes" : ["human", "rgb_array"], "render_fps" : 4}
+    metadata = {"render_modes" : ["human", "rgb_array"], "render_fps" : 300}
 
     def __init__(self, reward_loc=[5., 8.], angle_deltas=[.01745, .01745],
             link_lengths=[6., 4.]):
@@ -23,6 +23,7 @@ class Discrete2DoF(gym.Env):
             angle_deltas (2,): angle update when actioning joints 1 and 2 (rad)
             link_lengths (2,): Lengths of links 1 and 2 (m)
         """
+
         self.angle_deltas = np.array(angle_deltas)
         self.reward_loc = np.array(reward_loc)
         self.link_lengths = np.array(link_lengths)
@@ -47,14 +48,18 @@ class Discrete2DoF(gym.Env):
         }
 
         """
-        If human-rendering is used, `self.window` will be a reference
+        If human-rendering is used, `self.screen` will be a reference
         to the window that we draw to. `self.clock` will be a clock that is used
         to ensure that the environment is rendered at the correct framerate in
         human-mode. They will remain `None` until human-mode is used for the
         first time.
         """
-        self.window = None
+        self.screen = None
         self.clock = None
+        self.screen_width = 512  # Size of the pygame window, pixels
+        self.screen_height = 512  # Size of the pygame window, pixels
+        self.screen_scale = 20  # pixels per meter 
+        
 
     def _get_obs(self):
         """ Returns an 8x1 array with observations from current state. """
@@ -129,7 +134,7 @@ class Discrete2DoF(gym.Env):
             action (int) discrete action (0,1 = +/- joint 1; 2,3 = +/- joint 2)
 
         Returns:
-            observation (dict): joint_angs, joint_locs, reward_loc
+            observation (8x1 array): Observations from current state
             reward (float): Euclidean distance between Joint 2 and reward
             done (bool): Whether simulation has terminated (i.e. reward reached)
             info (dict): Auxiliary information about environment
@@ -150,13 +155,101 @@ class Discrete2DoF(gym.Env):
         """ Renders simulation for viewing. """
         #TODO Rendering will happen in ROS
         #TODO Perhaps make a simulation in pygame to sidestep ROS initially?
-        pass
+        import pygame # avoid global pygame dependency. 
+                      # This method is not called with no-render. 
+        black = (0, 0, 0)
+        green = (30, 200, 30)
+        red = (200, 30, 30)
+        cyan = (30, 150, 120)
+        white = (255, 255, 255)
+        #
+        canvas = pygame.Surface((self.screen_width, self.screen_height))
+        canvas.fill(white)
+        origin_px = np.array([self.screen_width // 2, self.screen_height // 2])
+        joint_1_px = origin_px + self.joint_locs[0] * [1,-1] * self.screen_scale
+        joint_2_px = origin_px + self.joint_locs[1] * [1,-1] * self.screen_scale
+        goal_ctr_px = origin_px + self.reward_loc * [1,-1] * self.screen_scale
+        goal_wd = 8
+        # Goal, cyan:
+        pygame.draw.rect(
+                canvas,
+                cyan,
+                (np.int32(goal_ctr_px)[0] - goal_wd//3, 
+                 np.int32(goal_ctr_px)[1] - goal_wd//3,
+                 goal_wd,
+                 goal_wd)
+        )
+        # Link 2, red:
+        pygame.draw.line(
+                canvas, 
+                red, 
+                np.int32(joint_1_px), 
+                np.int32(joint_2_px),
+                width=3
+        )
+        pygame.draw.circle(
+                canvas,
+                red,
+                np.int32(joint_2_px),
+                5
+        )      
+        # Link 1, green:
+        pygame.draw.line(
+                canvas, 
+                green, 
+                np.int32(origin_px), 
+                np.int32(joint_1_px),
+                width=5
+        )
+        pygame.draw.circle(
+                canvas,
+                green,
+                np.int32(joint_1_px),
+                5
+        )   
+        # Origin, black:
+        pygame.draw.circle(
+                canvas,
+                black,
+                np.int32(origin_px),
+                7
+        )               
+        if self.screen is None:
+            pygame.init()
+            pygame.display.init()
+            self.screen = pygame.display.set_mode((self.screen_width, 
+                                                   self.screen_height))
+        if self.clock is None:
+            self.clock = pygame.time.Clock()
+        
+        if mode == "human":
+            assert self.screen is not None
+            # The following line copies our drawings from `canvas` to 
+            # the visible window
+            self.screen.blit(canvas, canvas.get_rect())
+            pygame.event.pump()
+            pygame.display.update()
 
+            # We need to ensure that human-rendering occurs at the predefined 
+            # framerate. The following line will automatically add a delay to 
+            # keep the framerate stable.
+            self.clock.tick(self.metadata["render_fps"])
+        else:  # rgb_array
+            return np.transpose(
+                np.array(pygame.surfarray.pixels3d(canvas)), axes=(1, 0, 2)
+            )
+    
+    
     def close(self):
         """ Closes any open resources used by the environment. """
-        pass
+        if self.screen is not None:
+            import pygame
+            pygame.display.quit()
+            pygame.quit()
 
 if __name__ == '__main__':
-    environment = Discrete2DoF()
-    environment.reset()
-    environment.step(2)
+    env = Discrete2DoF()
+    env.reset()
+    for _ in range(10000):
+        env.render()
+        env.step(np.random.choice([0,1,2,3]))
